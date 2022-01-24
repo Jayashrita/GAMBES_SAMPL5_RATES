58138da688110d1384e515052f500001832f5af0
Jayashrita Debnath <jayashritadebnath@student-net-vpn-1909.intern.ethz.ch>
2022-01-24 09:15:43 +0100

    added_data

---
+++ OAMe-G4/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+module load new
+module load gcc/6.3.0
+module load open_mpi/3.0.0
+module load cmake/3.11.4
+module load fftw/3.3.4
+module load python/3.6.0
+#module load mkl/2018.1
+source ~/gromacs-2019.4-source/bin/GMXRC.bash
+source ~/plumed2-inve/sourceme.sh
+#source /cluster/home/debnathj/cp2k-tools/cp2k-code-r18596-trunk-cp2k-tools-toolchain/install/setup
+
+jname=${PWD##*/}
+
+bsub  -J $jname -W 48:00 -n 4 gmx_mpi_d mdrun -plumed plumed.dat -s topol.tpr
+#bsub  -J $jname -W 4:00 -n 1 -R "select[model==XeonGold_6150]" /cluster/home/debnathj/cp2k-6.1/exe/local/cp2k.sopt -i sn2.inp -o sn2.out
+#bsub  -J $jname -W 1:30 -n 1 plumed driver --plumed plumed_coordination.dat --ixyz Hydrobromane-pos-1.xyz --length-units A 
